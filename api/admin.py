2708ef0f799ac25b86b1000dfeaa7a9befc8ea0a
Axel Pélerin <arcanynka@gmail.com>
2022-03-21 00:14:33 +0100

    Initial commit

---
+++ api/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import InformationHub, Article, Journal, Category
+
+
+admin.site.register(InformationHub)
+admin.site.register(Article)
+admin.site.register(Journal)
+admin.site.register(Category)
---
+++ api/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+
+class Journal(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    body = models.JSONField()
+    link = models.CharField(max_length=255)
+    image_link = models.CharField(max_length=255)
+    journal = models.ForeignKey(Journal, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
+
+class InformationHub(models.Model):
+    title = models.CharField(max_length=255)
+    original_source_link = models.CharField(max_length=255)
+    articles = models.ManyToManyField(Article)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
