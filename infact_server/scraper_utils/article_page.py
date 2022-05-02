654f916ead42f9f365259ab8663a6457747dcce8
Axel Pélerin <pelerin.axel@gmail.com>
2022-05-02 13:59:10 +0200

    Add articles_pages.py

---
+++ infact_server/scraper_utils/article_page.py
@@ -0,0 +1,32 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+
+class ArticlePage:
+    """ Handle article page information scraping logic """
+    def __init__(self, article_url):
+        self.article_url = article_url
+
+
+    def get_article_title(self):
+        """ Retrieve article title """
+        logging.error("Not implemented")
+
+
+    def get_article_body(self):
+        """ Retrieve article body """
+        logging.error("Not implemented")
+
+
+    def get_article_author(self):
+        """ Retrieve article author """
+        logging.error("Not implemented")
+
+
+    def get_article_date(self):
+        """ Retrieve article date """
+        logging.error("Not implemented")
+
+
+    def get_article_links(self):
+        """ Retrieve article links """
+        logging.error("Not implemented")
