eeb3858e4d7a56d2348069e38b82484f57354452
Axel Pélerin <pelerin.axel@gmail.com>
2022-05-05 15:03:37 +0200

    Update scraper for Marianne Todo: Structurize the scraping

--- mag_web/scraper/spiders/modules/marianne/browser.py
+++ mag_web/scraper/spiders/modules/marianne/browser.py
@@ -1,3 +1,4 @@
+import scrapy
 from scrapy import Spider
 
 
@@ -8,6 +9,26 @@ class MarianneBrowser(Spider):
     name = "marianne"
     start_urls = ["https://www.marianne.net/"]
 
+    # response = None
+    # todo separate article from browser
+
     def parse(self, response, **kwargs):
+        # self.response = response
         articles_link = response.css('.articles-hot__list').css('.thumbnail__link::attr(href)').getall()
-        print(articles_link)
+        for articles_link in articles_link:
+            url = response.urljoin(articles_link)
+            yield scrapy.Request(url=url, callback=self.parse_article)
+
+    def parse_article(self, response):
+        # self.response = response
+        title = response.xpath('//h1[@class="article__heading"]/text()').get()
+        img_link = response.css('.article__header').css('.responsive-image::attr(src)').extract_first()
+        author = response.xpath('//address[@class="article__details"]//a[@rel="author"]/span/text()').get()
+
+    def get_body(self, response):
+        body = []
+        return body
+
+    def get_abstract(self, response):
+        abstract = response.xpath('//h2[@class="article__headline article__item"]/text()').get()
+        return abstract
