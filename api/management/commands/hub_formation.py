c5976153bf9c5131076839adbba4350f38314bf2
Axel Pélerin <pelerin.axel@gmail.com>
2022-05-06 13:21:36 +0200

    Add images to random list

--- api/management/commands/hub_formation.py
+++ api/management/commands/hub_formation.py
@@ -1,3 +1,5 @@
+import random
+
 from django.core.management import BaseCommand
 
 from api.models import Article, InformationHub
@@ -5,6 +7,12 @@ from api.models import Article, InformationHub
 
 class Command(BaseCommand):
     help = 'Create hub_information from articles in the database'
+    images_url = [
+        "https://www.thespruce.com/thmb/t13CIs9CH0HfuggdQ-DU9zk_QHo=/3780x2126/smart/filters:no_upscale()/do-ducks-have-teeth-4153828-hero-9614a7e9f4a049b48e8a35a9296c562c.jpg",
+        "https://www.cdiscount.com/pdt2/2/6/9/1/1200x1200/auc2009706636269/rw/244677-18cm-rainbow-cute-alpacasso-kawaii-alpaca-l.jpg",
+        "https://maxitacos.fr/wp-content/uploads/2019/09/coca-cherry-boite-1.png",
+        "https://www.jardiner-malin.fr/wp-content/uploads/2021/12/Cactophile-cactus-succulente.jpg"
+    ]
 
     def handle(self, *args, **options):
         self.stdout.write("Creating hubs...")
@@ -18,6 +26,6 @@ class Command(BaseCommand):
             sample_articles = articles.order_by('?')[:3]
             instance = InformationHub.objects.create(
                 title=sample_articles[0].title,
-                original_source_link="https://www.cdiscount.com/pdt2/2/6/9/1/1200x1200/auc2009706636269/rw/244677-18cm-rainbow-cute-alpacasso-kawaii-alpaca-l.jpg"
+                original_source_link=self.images_url[random.randint(0, self.images_url.__len__() - 1)],
             )
             instance.articles.add(*sample_articles)
