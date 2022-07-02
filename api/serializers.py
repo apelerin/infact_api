a9605ff88b88990f6f88a3eaab7886fc2aa0d111
Axel Pélerin <arcanynka@gmail.com>
2022-07-02 18:21:38 +0200

    Add id to items

--- api/serializers.py
+++ api/serializers.py
@@ -6,23 +6,23 @@ from api.models import Journal, Category, Article, InformationHub
 class JournalSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Journal
-        fields = ['name']
+        fields = ['name', 'id']
 
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
-        fields = ['name']
+        fields = ['name', 'id']
 
 
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Article
         fields = ['author', 'title', 'date', 'body', 'link',
-                  'image_link', 'journal', 'journal', 'categories']
+                  'image_link', 'journal', 'journal', 'categories', 'id']
 
 
 class InformationHubSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = InformationHub
-        fields = ['title', 'original_source_link', 'articles']
+        fields = ['title', 'original_source_link', 'articles', 'id']
